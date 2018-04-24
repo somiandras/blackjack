8df94a6bd980e7887cfd0a60e63ab1bb08755eb6
doktor_justice <somiandras@gmail.com>
2018-04-24 10:08:33 +0200

    Complete Card and Deck with unit tests

---
+++ blackjack/cards.py
@@ -0,0 +1,86 @@
+import random
+
+CARD_FACES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+SUIT_CHAR_CODES = {
+    'heart': u'\u2661',
+    'club': u'\u2667',
+    'spade': u'\u2664',
+    'diamond': u'\u2662'
+}
+
+
+class CardException(Exception):
+    pass
+
+
+class DeckException(Exception):
+    pass
+
+
+class Card:
+    '''Class defining individual playing cards in a deck.'''
+
+    def __init__(self, suit, rank=None, face=None):
+        assert suit in ['heart', 'club', 'spade', 'diamond'], 'Unkown suit'
+        self.suit = suit
+
+        assert rank or face, 'Either card face or rank has to be defined'
+        if rank:
+            assert rank in range(13), 'Invalid card rank'
+            self.rank = rank
+            self.face = CARD_FACES[rank]
+        elif face:
+            assert face in CARD_FACES, 'Invalid card face'
+            self.face = face
+            self.rank = CARD_FACES.index(face)
+
+    def __repr__(self):
+        return self.symbol
+
+    @property
+    def symbol(self):
+        return '{}{}'.format(SUIT_CHAR_CODES[self.suit], self.face)
+
+
+class Deck:
+    '''Class defining a deck of playing cards.'''
+
+    def __init__(self):
+        self.reshuffle()
+
+    def _generate(self):
+        '''Generate new deck of cards and return list of cards.'''
+
+        new_deck = []
+        for suit in SUIT_CHAR_CODES.keys():
+            for face in CARD_FACES:
+                new_deck.append(Card(suit, face=face))
+
+        return new_deck
+
+    def shuffle(self):
+        '''Shuffle remaining cards inplace.'''
+
+        random.shuffle(self.cards)
+
+    def reshuffle(self):
+        '''Get new deck and shuffle it inplace.'''
+        
+        self.cards = self._generate()
+        self.shuffle()
+        self.dealt = []
+
+    def deal(self, n_cards=1):
+        '''Get last n cards and return them in a list.'''
+
+        assert n_cards > 0, 'n_cards has to be greater than 0'
+
+        if n_cards > len(self.cards):
+            raise DeckException('Deck is shorter ({}) than needed ({})'
+                                .format(len(self.cards), n_cards))
+        dealt_cards = self.cards[-n_cards:]
+        dealt_cards.reverse()
+        self.cards = self.cards[:-n_cards]
+        self.dealt.extend(dealt_cards)
+
+        return dealt_cards
---
+++ blackjack/test_cards.py
@@ -0,0 +1,77 @@
+import unittest
+from cards import Card, Deck, CardException, DeckException
+
+
+class CardTest(unittest.TestCase):
+    def test_wrong_card_init(self):
+        kwargs = [
+            {'suit': 'heart', 'face': 'X'},
+            {'suit': 'green', 'face': 10},
+            {'suit': 'club', 'rank': 15},
+            {'suit': 'diamond'}
+        ]
+
+        for kwarg in kwargs:
+            with self.subTest(kwarg=kwarg):
+                with self.assertRaises(AssertionError):
+                    Card(**kwarg)
+
+    def test_correct_card_init(self):
+        kwargs = [
+            {'suit': 'heart', 'face': 'Q'},
+            {'suit': 'heart', 'rank': 10}
+        ]
+
+        for kwarg in kwargs:
+            with self.subTest(kwarg=kwarg):
+                card = Card(**kwarg)
+                self.assertEqual(card.symbol, '\u2661Q')
+
+
+class DeckTest(unittest.TestCase):
+    def setUp(self):
+        self.deck = Deck()
+
+    def test_deck_reshuffle(self):
+        self.assertIsNotNone(self.deck.cards)
+        self.assertListEqual(self.deck.dealt, [])
+
+    def test_generate_deck(self):
+        new_deck = self.deck._generate()
+        
+        self.assertIsInstance(new_deck, list)
+        self.assertEqual(len(set(new_deck)), 52)
+        self.assertIsInstance(new_deck[0], Card)
+
+    def test_shuffle(self):
+        old_deck = self.deck.cards.copy()
+        self.deck.shuffle()
+        new_deck = self.deck.cards.copy()
+
+        self.assertEqual(len(old_deck), len(new_deck))
+        self.assertNotEqual(old_deck, new_deck)
+        self.assertEqual(sum([1 for card in self.deck.cards if card in old_deck]), len(old_deck))
+
+    def test_deal(self):
+        for n in [0, 1, 2, 52]:
+            with self.subTest(n=n):
+                old_deck = self.deck.cards.copy()
+                
+                if n <=0:
+                    with self.assertRaises(AssertionError):
+                        self.deck.deal(n_cards=n)
+                elif n > len(old_deck):
+                    with self.assertRaises(DeckException):
+                        self.deck.deal(n_cards=n)
+                else:
+                    dealt = self.deck.deal(n_cards=n)
+                    dealt.reverse()
+
+                    self.assertEqual(len(self.deck.cards), len(old_deck) - n)
+                    self.assertEqual(len(dealt), n)
+                    self.assertListEqual(old_deck[-n:], dealt)
+                    self.assertNotIn(dealt[0], self.deck.cards)
+
+
+if __name__ == '__main__':
+    unittest.main()
