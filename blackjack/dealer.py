9e8d940e32429659a3f3e8be48f3a1d737174638
doktor_justice <somiandras@gmail.com>
2018-04-25 08:39:15 +0200

    Add basic dealer functionality
    Add game setup and reset and hand evaluation methods.

--- blackjack/dealer.py
+++ blackjack/dealer.py
@@ -1,7 +1,45 @@
+from blackjack.cards import Deck
+
+
 class DealerException(Exception):
     pass
 
 
 class Dealer:
+
     def __init__(self, player):
-        pass
+        self.player = player
+        self.deck = Deck()
+        self.reset()
+
+    def reset(self):
+        self.deck.reshuffle()
+        self.player_cards = []
+        self.house_cards = []
+        self.player_value = 0
+        self.house_value = 0
+
+    def deal(self):
+        self.player_cards = self.deck.deal(n_cards=2)
+        self.house_cards = self.deck.deal(n_cards=1)
+
+    def evaluate_hand(self, hand):
+        aces = [card for card in hand if card.face == 'A']
+        non_aces = [card for card in hand if card.face != 'A']
+
+        value = 0
+        for card in non_aces:
+            try:
+                card_value = int(card.face)
+            except ValueError:
+                card_value = 10
+
+            value += card_value
+        
+        for ace in aces:
+            if value <= 10:
+                value += 11
+            else:
+                value += 1
+
+        return value
--- tests/test_dealer.py
+++ tests/test_dealer.py
@@ -1,8 +1,41 @@
 import unittest
 from blackjack.dealer import Dealer, DealerException
-
+from blackjack.player import Player
+from blackjack.cards import Card
+from unittest.mock import MagicMock
 
 class TestDealer(unittest.TestCase):
 
     def setUp(self):
-        pass
+        mock_player = MagicMock(spec=Player)
+        self.dealer = Dealer(mock_player)
+
+    def test_deal(self):
+        self.dealer.deal()
+
+        self.assertEqual(len(self.dealer.player_cards), 2)
+        self.assertEqual(len(self.dealer.house_cards), 1)
+
+    def test_reset(self):
+        self.dealer.reset()
+
+        self.assertEqual(len(self.dealer.player_cards), 0)
+        self.assertEqual(self.dealer.player_value, 0)
+        self.assertEqual(self.dealer.house_value, 0)
+
+    def test_evaluate_hand(self):
+        hands = [
+            [Card('diamond', '3'), Card('club', '10')],
+            [Card('diamond', 'A'), Card('club', '10')],
+            [Card('diamond', 'A'), Card('club', '2')],
+            [Card('diamond', 'A'), Card('club', 'A')],
+            [Card('spade', 'K'), Card('spade', 'Q')],
+            [Card('spade', 'K'), Card('spade', 'Q'), Card('club', 'A')]
+        ]
+
+        values = [13, 21, 13, 12, 20, 21]
+
+        for hand, value in zip(hands, values):
+            with self.subTest(hand=hand, value=value):
+                calculated_value = self.dealer.evaluate_hand(hand)
+                self.assertEqual(calculated_value, value)
