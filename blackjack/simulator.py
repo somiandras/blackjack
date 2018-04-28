f98bbbbc85e53004e0cb808f9733136442ffdf6d
doktor_justice <somiandras@gmail.com>
2018-04-28 16:38:29 +0200

    Doc Simulator class

--- blackjack/simulator.py
+++ blackjack/simulator.py
@@ -3,12 +3,18 @@ from blackjack.player import Player
 from blackjack.logger import Logger
 
 
-class SimulatorException(Exception):
-    pass
-
-
 class Simulator:
+    '''
+    Class for running series of blackjack games with one player and some
+    parameters for configuring the simulation. Under the hood uses
+    `blackjack.Dealer` and `blackjack.Logger`.
 
+    Args:  
+    `player`: configured blackjack.Player instance which is added to the
+    underlying blackjack.Dealer object.  
+    `test_games`: number of rounds for testing the learning after 
+    training phase is finished
+    '''
     def __init__(self, player, test_games=100):
         self.player = player
         self.test_games = test_games
@@ -16,6 +22,16 @@ class Simulator:
         self.logger = Logger()
         
     def run(self):
+        '''
+        Runs the simulation in two phases: 1) training until the 
+        `player.training` flag is set to `True` 2) testing after training
+        flag is set to `False`.
+
+        After training and testing is done this method logs what the 
+        player has "learned" by logging `player.Q`.
+
+        Returns: self
+        '''
         rounds = 0
         while self.player.training:
             self.dealer.run_game()
@@ -26,6 +42,16 @@ class Simulator:
             rounds += 1
 
         self.logger.log_Q(self.player.Q)
+
+        return self
     
     def report(self, *args, **kwargs):
+        '''
+        Wrapper method for blackjack.logger.report(). All arguments are
+        passed unchanged.
+
+        Returns: self
+        '''
         self.logger.report(*args, **kwargs)
+
+        return self
