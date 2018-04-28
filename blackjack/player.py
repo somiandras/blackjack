29dc0a6fecc97ef3e66a968d33e81a84c844129c
doktor_justice <somiandras@gmail.com>
2018-04-28 11:00:26 +0200

    Add constant epsilon option to Player
    If epsilon is set to be constant then a fixed number of training rounds
    are completed before switching to testing

--- blackjack/player.py
+++ blackjack/player.py
@@ -12,11 +12,14 @@ class Player:
     transitions = []
     training = True
 
-    def __init__(self, alpha=0.8, gamma=0.9, epsilon=0.9, tolerance=0.01):
-        self.alpha = 0.5
+    def __init__(self, alpha=0.5, gamma=0.9, epsilon=0.9, no_decay=False,
+                tolerance=0.01, training_rounds=10000):
+        self.alpha = alpha
         self.gamma = gamma
         self._epsilon = epsilon
+        self.no_decay = no_decay
         self.tolerance = tolerance
+        self.training_rounds = training_rounds
 
     def action(self, player_cards, house_cards):
 
@@ -26,19 +29,20 @@ class Player:
 
         state = (tuple(player_cards), house_cards[0])
 
+        if state not in self.Q and self.training:
+            self.Q[state] = {'hit': 0, 'stand': 0}
+
         roll = random.random()
         if roll < self.epsilon or state not in self.Q:
             potential_actions = ['stand', 'hit']
         else:
             maxQ = max([value for key, value in self.Q[state].items()])
             potential_actions = [a for a, q in self.Q[state].items() if q == maxQ]
-            
+        
         action = random.choice(potential_actions)
-
+        
         if self.training:
             self.transitions.append((state, action))
-            if state not in self.Q:
-                self.Q[state] = {'hit': 0, 'stand': 0}
 
         return action
 
@@ -46,11 +50,11 @@ class Player:
         if self.training:
             self.learn(reward)
 
-        if self.epsilon < self.tolerance:
-            self.training = False
-
         self.transitions = []
         self.t += 1
+
+        if self.epsilon < self.tolerance or (self.no_decay and self.t > self.training_rounds):
+            self.training = False
     
     def learn(self, reward):
         rev_transitions = list(reversed(self.transitions))
@@ -67,7 +71,7 @@ class Player:
 
     @property
     def epsilon(self):
-        if self.training:
-            return self._epsilon ** self.t
+        if self.no_decay:
+            return self._epsilon * self.training
         else:
-            return 0
+            return (self._epsilon ** self.t) * self.training
