ff06061666c74228c4a1c5156f843b6ac491b6c8
doktor_justice <somiandras@gmail.com>
2018-04-28 16:46:22 +0200

    Doc Deck class

--- blackjack/deck.py
+++ blackjack/deck.py
@@ -1,36 +1,57 @@
 import random
 
-CARD_FACES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+CARD_RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+'''Possible card ranks for generating a deck'''
+
 SUITS = ['c', 'd', 'h', 's']
+'''Abreviations for the 4 card suits: clubs, diamonds, hearts and spades'''
+
 
 class DeckException(Exception):
+    '''Custom exception for `blackjack.Deck` class.'''
     pass
 
 
 class Deck:
-    '''Class defining a deck of playing cards.'''
+    '''
+    Class defining a deck of playing cards with necessary methods to deal
+    and (re)shuffle cards and track what cards had been dealt. Initializes 
+    a shuffled state.
+    '''
 
     def __init__(self):
         self.reshuffle()
 
     def _generate(self):
-        '''Generate new deck of cards and return list of cards.'''
+        '''
+        Generate new deck of cards and return list of cards.
+
+        Returns: list(str): list of cards in the form of 'As', '4c', etc.
+        '''
 
         new_deck = []
         for suit in SUITS:
-            for face in CARD_FACES:
+            for face in CARD_RANKS:
                 new_deck.append('{}{}'.format(face, suit))
 
         return new_deck
 
     def shuffle(self):
-        '''Shuffle remaining cards inplace.'''
+        '''
+        Shuffle remaining cards inplace in the deck.
+
+        Returns: self
+        '''
 
         random.shuffle(self.cards)
         return self
 
     def reshuffle(self):
-        '''Get new deck and shuffle it inplace.'''
+        '''
+        Get new deck and shuffle it inplace.
+
+        Returns: self
+        '''
         
         self.cards = self._generate()
         self.shuffle()
@@ -38,7 +59,12 @@ class Deck:
         return self
 
     def deal(self, n_cards=1):
-        '''Get last n cards and return them in a list.'''
+        '''
+        Gets last n cards from current deck and returns them in a list.
+        The `deck.dealt` list is extended with these cards.
+        
+        Returns: list(str): list of cards in the form of 'As', '4c', etc.
+        '''
 
         assert n_cards > 0, 'n_cards has to be greater than 0'
 
@@ -47,6 +73,7 @@ class Deck:
                                 .format(len(self.cards), n_cards))
         dealt_cards = self.cards[-n_cards:]
         dealt_cards.reverse()
+
         self.cards = self.cards[:-n_cards]
         self.dealt.extend(dealt_cards)
 
